d9e8a998aee0fea6c8fe63f3faeebb2618ef34ef
Luis Zenteno <ltzenteno@gmail.com>
2021-08-16 14:42:58 -0400

    db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 
+const DB_CONFIG: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [],
+  synchronize: true,
+};
+
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    TypeOrmModule.forRoot(DB_CONFIG),
+    UsersModule,
+    ReportsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
