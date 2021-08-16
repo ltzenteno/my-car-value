8073273f6fd8a4c654dc1be4583dcdf6cbd80e3f
Luis Zenteno <ltzenteno@gmail.com>
2021-08-16 16:00:15 -0400

    creating report entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/entity/users.entity';
+import { Report } from './reports/entity/report.entity';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
+// TODO: find a better solution to avoid importing entities directly in this file
+const ENTITIES: EntityClassOrSchema[] = [
+  User,
+  Report,
+];
 
 const DB_CONFIG: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: 'db.sqlite',
-  entities: [User],   // TODO: find a better solution to avoid importing the entity here
+  entities: ENTITIES,
   synchronize: true,
 };
 
--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
 import { ReportsController } from 'src/reports/controller/reports.controller';
 import { ReportsService } from 'src/reports/service/reports.service';
+import { Report } from './entity/report.entity';
+
+/**
+ * List that contains all entities for this module
+ * (it will create a Repository internally)
+ */
+const ENTITIES: EntityClassOrSchema[] = [
+  Report,
+]
 
 @Module({
   controllers: [ReportsController],
+  imports: [TypeOrmModule.forFeature(ENTITIES)],
   providers: [ReportsService]
 })
 export class ReportsModule {}
