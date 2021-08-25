dfd51dc526e262fe4e689ad80440793378184f00
Luis Zenteno <ltzenteno@gmail.com>
2021-08-24 22:27:13 -0400

    change imports to use relative path

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
-import { ReportsController } from 'src/reports/controller/reports.controller';
-import { ReportsService } from 'src/reports/service/reports.service';
+import { ReportsController } from './controller/reports.controller';
 import { Report } from './entity/report.entity';
+import { ReportsService } from './service/reports.service';
 
 /**
  * List that contains all entities for this module
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
-import { AuthController, UsersController } from 'src/users/controller';
-import { UsersService } from 'src/users/service/users.service';
+import { AuthController, UsersController } from './controller';
 import { User } from './entity/users.entity';
 import { CurrentUserInterceptor } from './interceptor/current-user.interceptor';
 import { AuthService } from './service/auth.service';
+import { UsersService } from './service/users.service';
 
 /**
  * List that contains all entities for this module
