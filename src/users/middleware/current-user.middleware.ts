9ca3ac3c33dfdb252c3429456780908aae27e767
Luis Zenteno <ltzenteno@gmail.com>
2021-09-06 15:18:53 -0400

    removing ts-ignore, adding property to existing Express interface

--- src/users/middleware/current-user.middleware.ts
+++ src/users/middleware/current-user.middleware.ts
@@ -1,6 +1,16 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { User } from '../entity/users.entity';
 import { UsersService } from '../service/users.service';
+
+// update / add additional property to the existing Express Request interface
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
 
 /**
  * NOTES: request order of execution:
@@ -16,7 +26,7 @@ export class CurrentUserMiddleware implements NestMiddleware {
 
     if (userId) {
       const user = await this.userService.findOne(userId);
-      // @ts-ignore
+
       req.currentUser = user;
     }
 
