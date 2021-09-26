da4ea5a15b88d713264a579f5ce313c5673b8428
Luis Zenteno <ltzenteno@gmail.com>
2021-09-25 22:43:50 -0400

    replacing SQL migration

--- migrations/1631502692303-initial-schema.js
+++ migrations/1631502692303-initial-schema.js
@@ -1,17 +1,64 @@
-const { MigrationInterface, QueryRunner } = require("typeorm");
+const { MigrationInterface, QueryRunner, Table } = require("typeorm");
 
 module.exports = class initialSchema1631502692303 {
     name = 'initialSchema1631502692303'
 
     async up(queryRunner) {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "isAdmin" boolean NOT NULL DEFAULT true, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`CREATE TABLE "report" ("id" SERIAL NOT NULL, "price" integer NOT NULL, "make" character varying NOT NULL, "model" character varying NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT false, "userId" integer, CONSTRAINT "PK_99e4d0bea58cba73c57f935a546" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "report" ADD CONSTRAINT "FK_e347c56b008c2057c9887e230aa" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-    }
+        await queryRunner.createTable(
+          new Table({
+            name: 'user',
+            columns: [
+              {
+                name: 'id',
+                type: 'integer',
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+              },
+              {
+                name: 'email',
+                type: 'varchar',
+              },
+              {
+                name: 'password',
+                type: 'varchar',
+              },
+              {
+                name: 'isAdmin',
+                type: 'boolean',
+                default: 'true',
+              },
+            ],
+          }),
+        );
 
-    async down(queryRunner) {
-        await queryRunner.query(`ALTER TABLE "report" DROP CONSTRAINT "FK_e347c56b008c2057c9887e230aa"`);
-        await queryRunner.query(`DROP TABLE "report"`);
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
+        await queryRunner.createTable(
+          new Table({
+            name: 'report',
+            columns: [
+              {
+                name: 'id',
+                type: 'integer',
+                isPrimary: true,
+                isGenerated: true,
+                generationStrategy: 'increment',
+              },
+              { name: 'approved', type: 'boolean', default: 'false' },
+              { name: 'price', type: 'float' },
+              { name: 'make', type: 'varchar' },
+              { name: 'model', type: 'varchar' },
+              { name: 'year', type: 'integer' },
+              { name: 'lng', type: 'float' },
+              { name: 'lat', type: 'float' },
+              { name: 'mileage', type: 'integer' },
+              { name: 'userId', type: 'integer' },
+            ],
+          }),
+        );
+      }
+
+      async down(queryRunner) {
+        await queryRunner.query(`DROP TABLE ""report""`);
+        await queryRunner.query(`DROP TABLE ""user""`);
+      }
 }
